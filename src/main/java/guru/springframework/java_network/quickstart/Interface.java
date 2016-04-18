09c897c971116e61743d101c7103733bdbfbf291
Ed Kerry <edwardkerry@gmail.com>
2016-04-18 07:56:35 +0100

    Adds Interface class and initial methods

--- src/main/java/guru/springframework/java_network/quickstart/Interface.java
+++ src/main/java/guru/springframework/java_network/quickstart/Interface.java
@@ -1,18 +1,54 @@
 package guru.springframework.java_network.quickstart;
 
-
 import static java.lang.System.*;
+import java.util.Scanner;
 
 public class Interface {
+
+
 
     public static void main(String[] args) {
         Interface inter=new Interface();
         inter.openingStatement();
+        inter.initialOptions();
     }
 
     public String openingStatement() {
-        String welcome="Hello, World\n";
-        out.print("Hello, World\n");
+        String welcome="You are standing in a social network\n";
+        out.print(welcome);
         return welcome;
     }
+
+    private static void initialOptions() {
+        out.print("To the NORTH you may LOG-IN\n");
+        out.print("To the SOUTH you may EXIT\n");
+        Scanner input = new Scanner(System.in);
+        String choice = input.next();
+        if (choice.equals("NORTH")) {
+            Interface.log_in();
+        } else if (choice.equals("SOUTH")) {
+            Interface.quit();
+        }
+        else { Interface.initialOptions(); }
+    }
+
+     private static void log_in(){
+         out.print("You come across a security checkpoint. Are you a NEW or EXISTING user?\n");;
+         Scanner input = new Scanner(System.in);
+         String choice = input.next();
+         if (choice.equals("NEW")) {
+             User.new();
+         } else if (choice.equals("EXISTING")) {
+             User.log_in();
+         }
+         else { Interface.log_in(); }
+    }
+
+    private static void quit(){
+        String leave = "exit";
+        out.print(leave);;
+    }
+
+
+
 }
--- src/main/test/guru/springframework/java_network/quickstart/InterfaceTest.java
+++ src/main/test/guru/springframework/java_network/quickstart/InterfaceTest.java
@@ -4,13 +4,22 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 
+
 public class InterfaceTest {
 
     Interface javaNet = new Interface();
 
     @Test
     public void testOpeningStatement() throws Exception {
-        String message = "Hello, World\n";
+        String message = "You are standing in a social network\n";
         assertEquals(message, javaNet.openingStatement());
     }
+
+//    @Test
+//    public void testNewUser() throws Exception {
+//        String name="Spike"
+//
+//        javaNet.createNewUser(name);
+//
+
 }
