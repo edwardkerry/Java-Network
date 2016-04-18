cb4394807d606e2eb488d698b0958318301c1851
Ed Kerry <edwardkerry@gmail.com>
2016-04-18 08:32:52 +0100

    Adds user registration

--- src/main/java/guru/springframework/java_network/quickstart/Interface.java
+++ src/main/java/guru/springframework/java_network/quickstart/Interface.java
@@ -5,8 +5,6 @@ import java.util.Scanner;
 
 public class Interface {
 
-
-
     public static void main(String[] args) {
         Interface inter=new Interface();
         inter.openingStatement();
@@ -19,36 +17,44 @@ public class Interface {
         return welcome;
     }
 
-    private static void initialOptions() {
+    private void initialOptions() {
         out.print("To the NORTH you may LOG-IN\n");
         out.print("To the SOUTH you may EXIT\n");
-        Scanner input = new Scanner(System.in);
-        String choice = input.next();
-        if (choice.equals("NORTH")) {
-            Interface.log_in();
-        } else if (choice.equals("SOUTH")) {
-            Interface.quit();
+        if (get_input().equals("NORTH")) {
+            log_in();
+        } else if (get_input().equals("SOUTH")) {
+            quit();
         }
-        else { Interface.initialOptions(); }
+        else { initialOptions(); }
     }
 
-     private static void log_in(){
-         out.print("You come across a security checkpoint. Are you a NEW or EXISTING user?\n");;
-         Scanner input = new Scanner(System.in);
-         String choice = input.next();
-         if (choice.equals("NEW")) {
-             User.new();
-         } else if (choice.equals("EXISTING")) {
-             User.log_in();
+     private void log_in(){
+         out.print("You come across a security checkpoint. Are you a NEW or EXISTING user?\n");
+         if (get_input().equals("NEW")) {
+             register();
+         } else if (get_input().equals("EXISTING")) {
+             log_in();
          }
-         else { Interface.log_in(); }
     }
 
-    private static void quit(){
+    private void register(){
+        out.print("What is your name?\n");
+        String name = get_input();
+        User user = new User(name)
+        out.print("Welcome " + user.name);
+    }
+
+    private void quit(){
         String leave = "exit";
         out.print(leave);;
     }
 
+
+    private String get_input(){
+        Scanner input = new Scanner(System.in);
+        String answer = input.next();
+        return answer;
+    }
 
 
 }
--- src/main/java/guru/springframework/java_network/quickstart/User.java
+++ src/main/java/guru/springframework/java_network/quickstart/User.java
@@ -1,7 +1,21 @@
 package guru.springframework.java_network.quickstart;
 
-/**
- * Created by lastmemories on 18/04/2016.
- */
+import static java.lang.System.*;
+
 public class User {
+
+    public String name;
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    public void register(){
+        out.print("User reached!" + name);
+    }
+
+    public void log_in() {
+        out.print("User reached!");
+    }
+
 }
